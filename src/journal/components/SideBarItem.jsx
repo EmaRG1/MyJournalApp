428ac769aec382af4e665ec3945e1ac685cc17e2
Emanuel <emanuelrojasguindan@gmail.com>
2025-02-27 12:28:54 -0300

    sidebar: flex column and body length fix

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -14,16 +14,20 @@ export const SideBarItem = ({ title, body, id, date, imageUrls = [] }) => {
   
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + '...' : title
-  },[title])
+  }, [title])
+  
+  const newBody = useMemo(() => {
+    return body.length > 17 ? body.substring(0, 17) + '...' : body
+  },[body])
   return (
     <ListItem  disablePadding>
         <ListItemButton onClick={ onClickNote }>
             <ListItemIcon>
                 <TurnedInNot />
             </ListItemIcon>
-            <Grid container>
-                <ListItemText primary={ newTitle } />
-                <ListItemText secondary={ body } />
+            <Grid container sx={{flexDirection: 'column'}}>
+                <ListItemText primary={newTitle} />
+                <ListItemText secondary={ newBody } />
             </Grid>
         </ListItemButton>
     </ListItem>
